18247b1a4f3ee9c242d5d36cca5b49ac998f9a28
fabricio jacob <fabrluc@outlook.com>
2023-10-11 22:55:01 -0300

    add

--- src/main/java/com/github/fabriciolfj/resources/GreetingResource.java
+++ src/main/java/com/github/fabriciolfj/resources/GreetingResource.java
@@ -1,5 +1,6 @@
-package com.github.fabriciolfj;
+package com.github.fabriciolfj.resources;
 
+import io.smallrye.common.annotation.RunOnVirtualThread;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -9,6 +10,7 @@ import jakarta.ws.rs.core.MediaType;
 public class GreetingResource {
 
     @GET
+    @RunOnVirtualThread
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
